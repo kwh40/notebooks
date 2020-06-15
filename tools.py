ddc56643072efd44135a8bb4cd35a46a4b1630cc
kwh40 <44206061+lenekaethe@users.noreply.github.com>
2020-06-15 15:00:31 +0200

    fix handler

--- tools.py
+++ tools.py
@@ -9,7 +9,7 @@ def print_with_timestamp(msg):
     )
 
 
-def checkMessageEncryption(msg):
+def check_message_encryption(msg):
     switcher = {
         # "pgp": "-",  # ----BEGIN PGP MESSAGE-----",
         # "error": "<",  #!DOCTYPE HTML PUBLIC \"-//W", #<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN"><title>500 Internal Server Error</title><h1>Internal Server Error</h1><p>The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application.</p>
@@ -18,4 +18,4 @@ def checkMessageEncryption(msg):
         "<": "error",
         "{": "msg",
     }
-    return switcher.get(msg[0], "Message starts with unknown character.")
+    return switcher.get(msg[1], "Message starts with unknown character.")
