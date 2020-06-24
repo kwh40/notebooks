9ea837b200f53b2c42beecf41f1644842a11adc1
kwh40 <44206061+lenekaethe@users.noreply.github.com>
2020-06-24 16:42:11 +0200

    validate error code

--- tools.py
+++ tools.py
@@ -15,7 +15,6 @@ def check_message_encryption(msg):
         # "error": "<",  #!DOCTYPE HTML PUBLIC \"-//W", #<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN"><title>500 Internal Server Error</title><h1>Internal Server Error</h1><p>The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application.</p>
         # "s3iMsg": "{",
         "-": "pgp",
-        "<": "error",
         "{": "msg",
     }
     return switcher.get(msg[1], "error")
