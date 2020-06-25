8eda57c60468adf59b7b7e8e3c6ef1c433640dd9
kwh40 <44206061+lenekaethe@users.noreply.github.com>
2020-06-25 14:28:58 +0200

    no public error callback

--- tools.py
+++ tools.py
@@ -8,12 +8,12 @@ def print_with_timestamp(msg):
         )
     )
 
+def error_callback(message):
+    print("An Error occured:", message)
+    print("==============================================================================")
 
 def check_message_encryption(msg):
     switcher = {
-        # "pgp": "-",  # ----BEGIN PGP MESSAGE-----",
-        # "error": "<",  #!DOCTYPE HTML PUBLIC \"-//W", #<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN"><title>500 Internal Server Error</title><h1>Internal Server Error</h1><p>The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application.</p>
-        # "s3iMsg": "{",
         "-": "pgp",
         "{": "msg",
     }
