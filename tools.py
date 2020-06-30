e31dd6c17a63d17ae841ccd4f5fa66a75035f6f1
kwh40 <44206061+lenekaethe@users.noreply.github.com>
2020-06-30 10:14:48 +0200

    fix string checker

--- tools.py
+++ tools.py
@@ -5,6 +5,7 @@ logging.basicConfig(level=logging.ERROR)
 yes = ["j", "ja", "Ja", "J" "Yes", "y", "yes", "Y"]
 no = ["n", "no", "No", "N", "nein", "Nein"]
 
+
 def print_with_timestamp(msg):
     print(
         "[S3I][{}]: {}".format(
@@ -12,12 +13,15 @@ def print_with_timestamp(msg):
         )
     )
 
+
 def error_callback(message):
     logging.debug(
         "[S3I][{}]: {}".format(
             time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), message
         ))
-    logging.debug("==============================================================================")
+    logging.debug(
+        "==============================================================================")
+
 
 def check_message_encryption(msg):
     switcher = {
@@ -26,14 +30,14 @@ def check_message_encryption(msg):
     }
     return switcher.get(msg[1], "error")
 
+
 def check_for_quotes(message):
-    if message[1] == '"' or message[-1] == '"':
+    if message[0] == '"' or message[-1] == '"':
         return message.strip('"')
     return message
 
+
 def check_for_spaces(text):
-    if text[1] == " " or text[-1] == " ":
+    if text[0] == " " or text[-1] == " ":
         return text.strip()
     return text
-
-
