f72d8865c787835630d45914482d14caa9838ab3
kwh40 <44206061+lenekaethe@users.noreply.github.com>
2020-07-01 18:15:55 +0200

    update after review

--- tools.py
+++ tools.py
@@ -1,9 +1,11 @@
 import time
 import logging
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(level=logging.DEBUG)
 
 yes = ["j", "ja", "Ja", "J" "Yes", "y", "yes", "Y"]
 no = ["n", "no", "No", "N", "nein", "Nein"]
+participants = ["ruecker", "kopetzky", "balindt", "kaulich", "ziesak",
+                "fien", "koch", "gebhard", "arboix", "user1_workshop", "user2_workshop", "user3_workshop", "wiepcke", "schluse", "rossmann"]
 
 
 def print_with_timestamp(msg):
@@ -15,6 +17,11 @@ def print_with_timestamp(msg):
 
 
 def error_callback(message):
+    logging.debug(
+        "[S3I][{}]: {}".format(
+            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()
+                          ), "An error occurred"
+        ))
     logging.debug(
         "[S3I][{}]: {}".format(
             time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), message
